54f372ace02a806eea6597cdf5643a5a192d3d69
pcsishun <pcsishun@Gmail.com>
2021-09-07 18:38:26 +0700

    create connection DB

---
+++ api/apiController.ts
@@ -0,0 +1,31 @@
+import bodyParser from "body-parser";
+import express from 'express';
+import dotenv from 'dotenv';
+import { connection } from '../config/connection.js'
+ 
+
+// const config = require('../config/connection.js')
+// const connection = config.connection;
+
+dotenv.config({ path: '../.env' });
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(bodyParser.json());
+
+
+
+app.get('/testnodeapi', (req, res) => {
+    connection.query('select * from testquerytable', function (error, result){
+        if (result){
+            // console.log(result)
+            res.send(result)
+        }else{
+            // console.log(error)
+            res.send(error)
+        }
+    }); 
+})
+
+
+app.listen(PORT, ()=> console.log(`Serve is running at port ${PORT}`));
